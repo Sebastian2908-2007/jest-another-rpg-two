6dee4da0aea72a5f053a4cdb138aef0217090de6
Sebabin <sebastiancrespin279@gmail.com>
2021-12-17 23:16:42 -0700

    game logic complete code refactored to use es6 syntax as well as enemy and player extending character for many properties and methods

--- _tests_/Enemy.test.js
+++ _tests_/Enemy.test.js
@@ -12,7 +12,7 @@ test('creates an enemy object', () => {
     expect(enemy.health).toEqual(expect.any(Number))
     expect(enemy.strength).toEqual(expect.any(Number));
     expect(enemy.agility).toEqual(expect.any(Number));
-    expect(enemy.Potion).toEqual(expect.any(Object));
+    expect(enemy.potion).toEqual(expect.any(Object));
 });
 
 
